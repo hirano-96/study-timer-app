ba20ac4ddd94c3dc42eceea6bf66a156a9a36d94
fuji <fujiyoshiyuki@gmail.com>
2023-06-12 23:01:27 +0900

    ポロモードアプリ完成

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = () => {
     const song = document.querySelector('.song');
+    const alarm = document.querySelector('.alarm')
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
 
@@ -8,19 +9,26 @@ const app = () => {
 
     // Time Display
     const timeDisplay = document.querySelector('.time-display');
-    const timeSelect = document.querySelectorAll('.time-select button')
+    const studyTimeSelect = document.querySelectorAll('.study-time-select button')
+    const restTimeSelect = document.querySelectorAll('.rest-time-select button')
+    const paternSelect = document.querySelectorAll('.patern-select button')
+
 
     // Get the Length of the outline
     const outlineLength = outline.getTotalLength();
-    console.log(outlineLength)
 
     //Duration
-    let fakeDuration = 600;
+    let fakeDuration = 0;
+    let studyTime = 0;
+    let restTime = 0;
+    let fakePatern = 'study'
+
+
 
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
-    // Pick different sounds
+    // 曲選択のボタン押下時に、HTML記載の参照mp3を変更
     sounds.forEach(sound => {
         sound.addEventListener('click', function(){
             song.src = this.getAttribute('data-sound');
@@ -28,19 +36,67 @@ const app = () => {
         });
     })
 
-    // Play Sound
+    // 再生ボタン押下時に、mp3ファイルを参照して実行
     play.addEventListener("click", () => {
         checkPlaying(song);
     });
 
-    // Select sound
-    timeSelect.forEach(option => {
+    // パターン選択
+    paternSelect.forEach(option => {
         option.addEventListener('click', function () {
-            fakeDuration = this.getAttribute('data-time');
-            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+            fakePatern = this.getAttribute('data');
+            if(fakePatern == 'study' ){
+                studyTimeSelect.forEach(option => {
+                    option.removeAttribute('disabled')
+                })
+                restTimeSelect.forEach(option => {
+                    option.setAttribute('disabled', true)
+                })
+                studyTimeSelect.forEach(option => {
+                    option.addEventListener('click', function () {
+                        fakeDuration = this.getAttribute('data-time');
+                        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+                    })
+                })
+            }
+            if(fakePatern == 'rest' ){
+                studyTimeSelect.forEach(option => {
+                    option.setAttribute('disabled', true)
+                })
+                restTimeSelect.forEach(option => {
+                    option.removeAttribute('disabled')
+                })
+                restTimeSelect.forEach(option => {
+                    option.addEventListener('click', function () {
+                        fakeDuration = this.getAttribute('data-time');
+                        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+                    })
+                })
+            }
+            if(fakePatern == 'both'){
+                studyTimeSelect.forEach(option => {
+                    option.removeAttribute('disabled')
+                })
+                restTimeSelect.forEach(option => {
+                    option.removeAttribute('disabled')
+                })
+                studyTimeSelect.forEach(option => {
+                    option.addEventListener('click', function () {
+                        studyTime = Math.floor(this.getAttribute('data-time'));
+                        fakeDuration = studyTime;
+                        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+                    })
+                })
+                restTimeSelect.forEach(option => {
+                    option.addEventListener('click', function () {
+                        restTime = Math.floor(this.getAttribute('data-time'));
+                        fakeDuration = studyTime + restTime;
+                        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+                    })
+                })
+            }
         })
     })
-
 
     // Create a function specific to stop and play the sounds
     const checkPlaying = song => {
@@ -66,15 +122,31 @@ const app = () => {
         
         timeDisplay.textContent = `${minutes}:${seconds}`;
 
-        if(currentTime >= fakeDuration){
-            song.pause();
-            song.currentTime = 0;
-            play.src = './svg/play.svg';
-            video.pause();
+        if(fakePatern == 'both'){
+            if(currentTime >= studyTime){
+                song.muted = true;
+                alarm.play();
+                if(currentTime >= studyTime + 3){
+                    alarm.pause();
+                    alarm.currentTime = 0;
+                }
+            }
+
         }
+        if(currentTime >= fakeDuration - 3){
+            song.muted = true;
+            alarm.play();
+            if(currentTime >= fakeDuration){
+                song.muted = false
+                song.pause();
+                song.currentTime = 0;
+                alarm.pause();
+                alarm.currentTime = 0;
+                play.src = './svg/play.svg';
+            }
+        }
+
     }
-
-
 };
 
 app();
